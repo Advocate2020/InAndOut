279702aacb9d22174a744202cbfe24d3570bd61e
sk_code <swartbooisakhanya@gmail.com>
2022-02-14 16:41:36 +0200

    InAndOut First Commit

---
+++ InAndOut/Controllers/ExpenseController.cs
@@ -0,0 +1,114 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        //enables dependency injection: this code allows you to use the database in your code.
+        private readonly ApplicationDbContext _db;
+
+        public ExpenseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //end of dependency injection
+
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objList = _db.Expenses;//allows you to get all the expenses in your database
+            return View(objList);
+        }
+
+        //Get-Create: for displaying the view
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post-Create: for saving view data
+        [HttpPost] //signals thats its a post method
+        [ValidateAntiForgeryToken]// only allows people who are authenticated to post
+        public IActionResult Create(Expense obj /**/)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Expenses.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        //Delete-Get: for getting delete view data       
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Expenses.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //Delete - post: for saving view data
+        [HttpPost] //signals thats its a post method
+        [ValidateAntiForgeryToken]// only allows people who are authenticated to post
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Expenses.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Get-Update: for getting update view data       
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //Post-Update: for saving view data
+        [HttpPost] //signals thats its a post method
+        [ValidateAntiForgeryToken]// only allows people who are authenticated to post
+        public IActionResult Update(Expense obj /**/)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Expenses.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+
+    }
+}
---
+++ InAndOut/Controllers/ItemController.cs
@@ -0,0 +1,44 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        //enables dependency injection: this code allows you to use the database in your code.
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //end of dependency injection
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;//allows you to get all the items in your database
+            return View(objList);
+        }
+
+        //Get-Create: for displaying the view
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post-Create: for saving view data
+        [HttpPost] //signals thats its a post method
+        [ValidateAntiForgeryToken]// only allows people who are authenticated to post
+        public IActionResult Create(Item obj /**/)
+        {
+            _db.Items.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
